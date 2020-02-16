d6205f6f24a2948f529f4a403df9426fab286262
Real Awesomeness <RealAwesomeness@outlook.com>
2020-02-15 21:18:00 -0500

    testing different tcp server using asyncio

--- main.py
+++ main.py
@@ -51,7 +51,8 @@ class poolFramework:
 
         for config in pool_configs:
             self.log.info("Initialized " + str(config['coin']) + " stratum")
-            Stratum(self.config, config, self.log, self.ssl_context)
+            #main(self.config, config, self.log, self.ssl_context)
+            asyncio.run(main(self))
 
     def loadSSL(self):
         ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
@@ -61,29 +62,52 @@ class poolFramework:
         ssl_context.set_ciphers("ECDHE+AESGCM")
         ssl_context.load_cert_chain(certfile=self['config.ssl_cert_path'], keyfile=self['config.ssl_keyfile_path'])
         ssl_context.set_alpn_protocols(["h2"])
-
-class Stratum:
-    def __init__(self, main_config, config, log, ssl_context):
-        self.main_config = main_config
-        self.config = config
-        self.log = log
-        self.ssl_context = ssl_context
-        self.template = {"error": None, "id": 0, "result": True}
-        asyncio.set_event_loop_policy(aiozmq.ZmqEventLoopPolicy())
-        asyncio.get_event_loop().run_until_complete(self.main())
-
-    @method
-    class mining:
-        async def authorize(self, username, password):
-            return_json = self.template
-            return_json.id = 2
-            return json.dumps(json.dumps(return_json))
-
+    
     async def main(self):
-        rep = await aiozmq.create_zmq_stream(zmq.REP, bind="tcp://" + str(self.main_config['ip'] + ":" + str(self.config['port'])))
-        while True:
-            request = await rep.read()
-            response = await dispatch(request[0].decode())
-            rep.write((str(response).encode(),))
+        #rep = await aiozmq.create_zmq_stream(zmq.REP, bind="tcp://" + str(self.main_config['ip'] + ":" + str(self.config['port'])))
+        #while True:
+        #    request = await rep.read()
+        #    response = await dispatch(request[0].decode())
+        #    rep.write((str(response).encode(),))
+        loop = asyncio.get_running_loop()
+
+        server = await loop.create_server(
+            lambda: EchoServerProtocol(),
+            self.main_config['ip'], self.config['port'])
+
+        async with server:
+            await server.serve_forever()
+
+#class Stratum:
+#    def __init__(self, main_config, config, log, ssl_context):
+#        self.main_config = main_config
+#        self.config = config
+#        self.log = log
+#        self.ssl_context = ssl_context
+#        self.template = {"error": None, "id": 0, "result": True}
+#        asyncio.set_event_loop_policy(aiozmq.ZmqEventLoopPolicy())
+#        asyncio.get_event_loop().run_until_complete(self.main())
+
+#    @method
+#    class mining:
+#        async def authorize(self, username, password):
+#            return_json = self.template
+#            return_json.id = 2
+#            return json.dumps(json.dumps(return_json))
+class EchoServerProtocol(asyncio.Protocol):
+    def connection_made(self, transport):
+        peername = transport.get_extra_info('peername')
+        print('Connection from {}'.format(peername))
+        self.transport = transport
+
+    def data_received(self, data):
+        message = data.decode()
+        print('Data received: {!r}'.format(message))
+
+        print('Send: {!r}'.format(message))
+        self.transport.write(data)
+
+        print('Close the client socket')
+        self.transport.close()
 
 poolFramework()
