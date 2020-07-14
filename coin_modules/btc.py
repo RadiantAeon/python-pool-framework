91212bfb2a39b9deaef0d69dba841dd5b96ee243
RealAwesomeness <RealAwesomeness@outlook.com>
2020-07-14 19:19:04 -0400

    twisted migration part 3

--- coin_modules/btc.py
+++ coin_modules/btc.py
@@ -6,122 +6,49 @@ import binascii
 import hashlib
 import multiprocessing
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
-from twisted.internet.protocol import Protocol, Factory
+from twisted.internet.protocol import Factory
+from twisted.protocols.basic import LineReceiver
 from twisted.internet import reactor
-from twisted.internet.endpoints import TCP4ServerEndpoint
 
 
-class TCPServer(Protocol):
+# "Client simply opens TCP socket and writes requests to the server in the form of JSON messages finished by the newline character \n" - slushpool
+# Therefore, this is a newline seperated protocol, so we should use line reciever
+class TCPServer(LineReceiver):
     # quote wiki "The factory must be passed to {protocol name}.__init__ when creating a new instance. The factory is used to share state that exists beyond the lifetime of any given connection."
     # see https://twistedmatrix.com/documents/current/core/howto/servers.html
+    # each connection is independent of each other inside the protocol class so the class variables in the protocol class are local to the connection
+    # only factory vars are "global"
     def __init__(self, factory):
         self.factory = factory
+        self.authorized = False
 
     def connectionMade(self):
-        self.factory.curr_job_id += 1
+        # we won't ever reach this amount of connections at a time - this is also the plain integer limit
+        self.factory.curr_job_id = (self.factory.curr_job_id + 1) % 9223372036854775807
         self.factory.log.debug("New connection from {}".format(self.client_address))
     
-    def dataRecieved(self, data):
-        responses = self.factory.handle_data(data)
-        for response in responses:
-            self.transport.write(response + "\n")
-
-
-'''def listen(client, address, job_id):
-    global log
-    global response_template
-    global block_height
-    global curr_job
-    size = 1024
-    cached_block_height = []
-    recieve = multiprocessing.Process(target=client.recv, name="recieve", args=(size)) # socketserver doesn't let you use timeout https://stackoverflow.com/questions/15705948/python-socketserver-timeout
-    while True:
-        try:
-            data = recieve.start()
-            log.debug(data)
-            if data:
-                # send the message to the handler below
-                response = handle_message(data, address)
-                client.send(response)
-            else:
-                log.debug("Client " + str(address) + " disconnected")
-                raise Exception("Client disconnected")
-            if block_height != cached_block_height:
-                response = response_template
-                response['error'] =  None
-                job = curr_job
-                job[0] = job_id
-                response['params'] = curr_job
-                client.send(json.dumps(response))
-        except:
-            client.close()
-            return False'''
-
-class StratumProtocol(Factory):
-
-    # tells twisted the protocol we want to use
-    protocol = TCPServer
-
-    def __init__(self, config, global_config, mongodb_connection, log):
-        # set values to variables that we will be using
-        # variables with no comments do exactly what the variable name implies
-        self.log = log
-        self.mongodb_connection = mongodb_connection
-        self.clients = {}
-        self.transport_num = 0
-        self.config = config
-        self.global_config = global_config
-        # generic stratum protocol response
-        self.response_template = config['stratum']['response_template']
-        # this is just a variable to add onto so all handler threads can tell when there is a new block
-        self.block_height = 0
-        # variable that gets incremented to make a unique id for each connection
-        self.curr_job_id = 1
-        self.job_template = [
-            0,  # job_id - ID of the job. Use this ID while submitting share generated from this job.
-            0,  # prevhash - Initial part of coinbase transaction.
-            0,  # coinb1 - Initial part of coinbase transaction.
-            0,  # coinb2 - Final part of coinbase transaction.
-            [],
-            # merkle_branch - List of hashes, will be used for calculation of merkle root. This is not a list of all transactions, it only contains prepared hashes of steps of merkle tree algorithm.
-            0,  # version - Bitcoin block version.
-            0,  # nbits - Encoded current network difficulty
-            0,  # ntime - Current ntime/
-            True  # clean_jobs - When true, server indicates that submitting shares from previous jobs don't have a sense and such shares will be rejected. When this flag is set, miner should also drop all previous jobs, so job_ids can be eventually rotated.
-        ]
-        # connects to bitcoin daemon with settings from config
-        self.rpc_connection = AuthServiceProxy("http://%s:%s@%s:%s" % (
-            config['daemon']["rpc_username"], config['daemon']["rpc_password"],
-            config['daemon']["daemon_ip"],
-            config['daemon']["daemon_port"]))
-        log.debug(config["coin"] + " init complete")
-
-    def handle_data(self, data, address):
-        
-        # in case the data contains multiple messages
-        messages = data.split('\n')
-        responses = []
-        for message in messages:
-            responses.append(handle_message(message, address))
-        return responses
+    # will contain cleanup in the future - for now just print a message
+    def connectionLost(self, reason):
+        self.factory.log.debug("Lost connection from {} because of {}".format(self.client_address, reason))
+    
+    def lineRecieved(self, line):        
+        self.transport.write(self.handle_message(line, self.client_address))
 
     def handle_message(self, data, address):
 
-        # oh i remember but this doesnt work rn
-        #authorized = False # We don't need to read from the db every time to check
-
         def authorize():
             params = message["params"]
-            response = response_template
+            response = self.factory.response_template
             # params format for mining.authorize should be in the format of ["slush.miner1", "password"] according to slush pool docs
             if self.mongodb_connection.find_one({"user": params[0], "password": params[1]}):
                 response["result"] = True
-                authorized = True
-                log.debug("Authorized user {}".format(params[0]))
+                self.authorized = True
+                self.factory.log.debug("Authorized user {}".format(params[0]))
             else:
                 response["result"] = False
                 response["error"] = "Unauthorized"
-                log.debug("Failed login by use {}".format(params[0]))
+                # we don't need to set self.authorized to False because its redundant
+                self.factory.log.debug("Failed login by use {}".format(params[0]))
             return response
 
         # need to make it so blocknotify isn't attackable - currently anyone can call it to fuck with us
@@ -129,12 +56,12 @@ class StratumProtocol(Factory):
             # adds one to the block_num var which each thread checks for so we can broadcast new jobs
             curr_job = self.job_template
             try:
-                blocktemplate = rpc_connection.getblocktemplate
+                blocktemplate = self.factory.rpc_connection.getblocktemplate
                 if blocktemplate["error"]:
                     raise Exception("Failed getblocktemplate rpc call! Is the bitcoin daemon running?")
-                log.info("New block at height {}".format(blocktemplate["result"]["height"]))
+                self.factory.log.info("New block at height {}".format(blocktemplate["result"]["height"]))
             except Exception as error:
-               log.error(error)
+               self.factory.log.error(error)
             curr_job[1] = blocktemplate["result"]["previousblockhash"]
             coinbase = binascii.a2b_hex(blocktemplate['coinbasetxn']['data'])
             extradata = b'yeet'
@@ -171,24 +98,61 @@ class StratumProtocol(Factory):
         try:
             message = json.loads(message)
         except:
-            log.debug("Recieved invalid json from {}".format(address))
+            self.factory.log.debug("Recieved invalid json from {}".format(address))
             raise ValueError('Invalid json was received from') # We can raise an error because of the try except in the listner
 
         # otherwise send the message to the correct method
         else:
-            if authorized and message["method"] != "mining.authorize":
+            if self.authorized and message["method"] != "mining.authorize":
                 try:
                     response = methods[message["method"]]() # no arguments because subfunctions can access parent function vars
                 except:
-                    response = response_template
+                    response = self.factory.response_template
                     response["error"] = "Invalid method!"
             else:
                 response = methods["mining.authorize"]
         return (json.dumps(response).encode("utf-8"))
 
+# contains all the "global" vars for the Twisted tcp server
+class StratumProtocol(Factory):
+
+    # tells twisted the protocol we want to use
+    protocol = TCPServer
+
+    def __init__(self, config, global_config, mongodb_connection, log):
+        # set values to variables that we will be using
+        # variables with no comments do exactly what the variable name implies
+        self.log = log
+        self.mongodb_connection = mongodb_connection
+        self.config = config
+        self.global_config = global_config
+        # generic stratum protocol response
+        self.response_template = config['stratum']['response_template']
+        # this is just a variable to add onto so all handler threads can tell when there is a new block
+        self.block_height = 0
+        # variable that gets incremented to make a unique id for each connection
+        self.curr_job_id = 1
+        self.job_template = [
+            0,  # job_id - ID of the job. Use this ID while submitting share generated from this job.
+            0,  # prevhash - Initial part of coinbase transaction.
+            0,  # coinb1 - Initial part of coinbase transaction.
+            0,  # coinb2 - Final part of coinbase transaction.
+            [],
+            # merkle_branch - List of hashes, will be used for calculation of merkle root. This is not a list of all transactions, it only contains prepared hashes of steps of merkle tree algorithm.
+            0,  # version - Bitcoin block version.
+            0,  # nbits - Encoded current network difficulty
+            0,  # ntime - Current ntime/
+            True  # clean_jobs - When true, server indicates that submitting shares from previous jobs don't have a sense and such shares will be rejected. When this flag is set, miner should also drop all previous jobs, so job_ids can be eventually rotated.
+        ]
+        # connects to bitcoin daemon with settings from config
+        self.rpc_connection = AuthServiceProxy("http://%s:%s@%s:%s" % (
+            config['daemon']["rpc_username"], config['daemon']["rpc_password"],
+            config['daemon']["daemon_ip"],
+            config['daemon']["daemon_port"]))
+        log.debug(config["coin"] + " init complete")
+
 
 # called by main to start the server thread
 def init_server(config, global_config, mongodb_connection, log):
-    endpoint = TCP4ServerEndpoint(reactor, config['port'])
-    endpoint.listen(StratumProtocol(config, global_config, mongodb_connection, log))
+    reactor.listenTCP(config.port, StratumProtocol(config, global_config, mongodb_connection, log))
     reactor.run()
